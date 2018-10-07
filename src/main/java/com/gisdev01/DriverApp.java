52f0d5a8bb6eceb2901dcd1403c154f55c79572e
gisdev01 <gisdev01@gmail.com>
2018-10-07 17:07:42 -0400

    Add logger

--- src/main/java/com/gisdev01/DriverApp.java
+++ src/main/java/com/gisdev01/DriverApp.java
@@ -3,13 +3,22 @@ package com.gisdev01;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.ToolRunner;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
+
 public class DriverApp {
 
     public static void main(String[] args) throws Exception {
+        Configurator.setRootLevel(Level.DEBUG);
+        Logger logger = LogManager.getLogger("WCountMR");
+
         int response = ToolRunner.run(new Configuration(),
                 new WCount(),
                 args);
-
+        logger.info("Tool Response Status Code: " + response);
         System.exit(response);
     }
 }
