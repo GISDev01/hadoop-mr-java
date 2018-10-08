ce906eaefe1a1fb50073f5ebf521995048de221a
gisdev01 <gisdev01@gmail.com>
2018-10-08 13:28:40 -0400

    Add new functionality to only perform counts on specific strings of interest

--- src/main/java/com/gisdev01/WCount.java
+++ src/main/java/com/gisdev01/WCount.java
@@ -16,35 +16,61 @@ import org.apache.hadoop.util.Tool;
 
 import java.io.File;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
 public class WCount extends Configured implements Tool {
 
     private String INPUT_PATH = "test/resources/in";
     private String OUTPUT_PATH = "test/resources/out";
+    private Logger logger = LogManager.getRootLogger();
 
-    public static class MapClass extends
+    private static Boolean SINGLE_WORD_FILTER_BOOL = true;
+    private static String SINGLE_WORD_FILTER = "specialword4";
+
+
+    public static class TokenizeMapper extends
             Mapper<Object, Text, Text, IntWritable> {
 
         private static final IntWritable ONE = new IntWritable(1);
         private Text word = new Text();
-
+        private Logger logger = LogManager.getRootLogger();
+        private Integer lineCount = 1;
 
         @Override
         protected void map(Object key, Text value, Context context)
                 throws IOException, InterruptedException {
 
+            //tokenize each line within each file detected in the input directory
             StringTokenizer tokenizer = new StringTokenizer(value.toString());
             while (tokenizer.hasMoreTokens()) {
                 String token = tokenizer.nextToken();
-                word.set(token);
-                context.write(word, ONE);
+                logger.debug("Mapper found token: " + token);
+                if (SINGLE_WORD_FILTER_BOOL) {
+                    if (SINGLE_WORD_FILTER.equals(token)) {
+                        word.set(token);
+                        context.write(word, ONE);
+                    }
+                }
+                else {
+                    word.set(token);
+                    context.write(word, ONE);
+                }
+
+
             }
+            logger.info("Mapper: line {}, read complete.", lineCount);
+
+            lineCount++;
         }
     }
 
-    public static class Reduce extends
+    public static class CountSumReducer extends
             Reducer<Text, IntWritable, Text, IntWritable> {
 
         private IntWritable count = new IntWritable();
+        private Logger logger = LogManager.getRootLogger();
 
         @Override
         protected void reduce(Text key, Iterable<IntWritable> values,
@@ -56,22 +82,33 @@ public class WCount extends Configured implements Tool {
             }
             count.set(sumCount);
             context.write(key, count);
+
+            logger.info("CountSumReducer Complete");
+
         }
     }
 
+    /*
+    Remove old data from the hadoop output directory before the next tool run
+     */
     private void deleteDir(File dir) {
+        logger.info("Deleting directory: " + dir.toString());
         File[] files = dir.listFiles();
 
-        for (File myFile: files) {
-            if (myFile.isDirectory()) {
-                deleteDir(myFile);
+        if (files != null) {
+            for (File existingFile: files) {
+                if (existingFile.isDirectory()) {
+                    deleteDir(existingFile);
+                }
+                existingFile.delete();
             }
-            myFile.delete();
-
         }
         dir.delete();
     }
 
+    /*
+    Called from Driver App by the Tool Runner
+     */
     public int run(String[] arg0) throws Exception {
         Job job = new Job(getConf());
         job.setJarByClass(WCount.class);
@@ -80,8 +117,8 @@ public class WCount extends Configured implements Tool {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
-        job.setMapperClass(MapClass.class);
-        job.setReducerClass(Reduce.class);
+        job.setMapperClass(TokenizeMapper.class);
+        job.setReducerClass(CountSumReducer.class);
 
         FileInputFormat.setInputPaths(job, new Path(INPUT_PATH));
 
@@ -92,6 +129,9 @@ public class WCount extends Configured implements Tool {
         FileOutputFormat.setOutputPath(job, new Path(OUTPUT_PATH));
 
         boolean success = job.waitForCompletion(true);
+        logger.info("MR Job Success: " + success);
+
+
         return success ? 0 : 1;
     }
 
